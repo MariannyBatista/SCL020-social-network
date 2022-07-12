02672539be56b62347250c60b3d87da493f40906
Marianny Batista <102048805+MariannyBatista@users.noreply.github.com>
2022-07-12 11:12:57 -0400

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCPBWucnyjp04RYeuXHeqT5f6pUtZ7EJOU",
+  authDomain: "seshatlab-a6921.firebaseapp.com",
+  projectId: "seshatlab-a6921",
+  storageBucket: "seshatlab-a6921.appspot.com",
+  messagingSenderId: "1049442824034",
+  appId: "1:1049442824034:web:a8ef1b80b3646818c4c63b",
+}
